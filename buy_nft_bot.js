a4f36d75675a112fd2aa2e3ad1b74da100871f18
Andrey Sakharov <sakharov.a@n-t.io>
2022-07-31 12:55:56 +0300

    using only websocket provider

--- buy_nft_bot.js
+++ buy_nft_bot.js
@@ -2,18 +2,16 @@ const Web3 = require('web3');
 
 class TransactionChecker {
     web3;
-    web3ws;
     account;
     subscription;
 
     constructor(projectId, account) {
-        this.web3ws = new Web3(new Web3.providers.WebsocketProvider('wss://rinkeby.infura.io/ws/v3/' + projectId));
-        this.web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/v3/' + projectId));
+        this.web3 = new Web3(new Web3.providers.WebsocketProvider('wss://rinkeby.infura.io/ws/v3/' + projectId));
         this.account = account.toLowerCase();
     }
 
     subscribe(topic) {
-        this.subscription = this.web3ws.eth.subscribe(topic, (err, res) => {
+        this.subscription = this.web3.eth.subscribe(topic, (err, res) => {
             if (err) console.error(err);
         });
     }
